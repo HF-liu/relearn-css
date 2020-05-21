0836cfc333c1a05ba3c42a2c73a62c2021b5ad21
David.L <liuhf24@gmail.com>
2020-05-21 14:43:44 -0700

    Day17

---
+++ scripts/17.js
@@ -0,0 +1,7 @@
+const placeholder = document.getElementById('placeholderText')
+
+for (i = 0; i < 30; i++) {
+  let newp = document.createElement('p')
+  newp.innerHTML = 'Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.'
+  placeholder.appendChild(newp)
+}
